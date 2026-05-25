dde16f53cecb962120c9bbf5784501bcddc831e9
Maksym Potseluiev <maksym.potseluiev@appflame.com>
2026-05-26 00:15:19 +0300

    Code_quality

---
+++ eslint.config.js
@@ -0,0 +1,26 @@
+import js from '@eslint/js';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import globals from 'globals';
+
+export default [
+  {
+    ignores: ['node_modules/**', '.npm-cache/**'],
+  },
+  js.configs.recommended,
+  eslintConfigPrettier,
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: {
+        ...globals.node,
+        ...globals.es2021,
+      },
+    },
+    rules: {
+      'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      'no-console': 'off',
+    },
+  },
+];
